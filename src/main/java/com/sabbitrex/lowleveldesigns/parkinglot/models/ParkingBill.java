d76498d3cd76546d425d58a4579a70762de9e1ce
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-08-19 17:00:40 +0530

    Add files via upload

---
+++ src/main/java/com/sabbitrex/lowleveldesigns/parkinglot/models/ParkingBill.java
@@ -0,0 +1,26 @@
+package com.sabbitrex.lowleveldesigns.parkinglot.models;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author Sharad
+ *
+ */
+@Builder
+public class ParkingBill {
+
+	@Getter
+	LocalDateTime entryTime;
+	
+	@Getter
+	LocalDateTime exitTime;
+	
+	@Getter
+	int billAmount;
+	
+	@Getter
+	ParkingSpot parkingSpot;
+}
